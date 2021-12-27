f35becffa6ad2dafd0d13ccfb55107ff46e10b4c
Māris Samats <m.samats@lvm.lv>
2021-12-27 16:23:35 +0200

    Updated styles, fixed bugs

--- treesim/Components/Controls.d.ts
+++ treesim/Components/Controls.d.ts
@@ -1,3 +1,5 @@
 import * as React from "react";
+import { IAnimation } from "./ExamplesContext";
 export declare const Controls: React.FC;
 export declare const UploadControls: React.FC;
+export declare const getFramesFromUrlForm: (formData: FormData) => Promise<IAnimation>;
